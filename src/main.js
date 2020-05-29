cd1213f6daf708451ce4f17769e8c83371f31e09
Sapaev Shukur <shukur_06@mail.ru>
2020-05-29 14:09:24 +0500

    Revert "[+] Header Component"
    This reverts commit da2127cd25deabd134c9c4e4234216b695b1f416.

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import jQuery from 'jquery'
-global.$ = jQuery
-import 'bootstrap'
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="home">
-    <div class="container">
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore eaque obcaecati facere facilis? Totam amet repellendus sit, reprehenderit cupiditate natus unde maiores soluta praesentium ullam assumenda deleniti numquam non a.
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit laudantium autem beatae praesentium nulla nemo ad pariatur ut! Sunt, distinctio recusandae! In unde, commodi ut ex iure pariatur dignissimos, voluptate autem quo suscipit laboriosam beatae cum? Non necessitatibus omnis iste repellat culpa exercitationem? Sapiente quaerat dolores ipsa aut! Dolore, culpa?
-      </p>
-    </div>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: "Home",
-  components: {}
-};
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
 </script>
